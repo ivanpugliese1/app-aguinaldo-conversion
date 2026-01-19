9dc8d7f5a0ab9563b79f71058fe7918987145ea3
Ivan Pugliese <ivanpugliese.f@gmail.com>
2026-01-19 15:55:46 -0300

    Estructura basica del proyecto y Header estilizado

--- src/main.js
+++ src/main.js
@@ -1,24 +1,5 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// Funcionalidad del menu toggle
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+const header = document.querySelector('header'); // Traigo el header completo.
+const menuToggle = document.querySelector('.menu-toggle'); // Traigo el botón que abre y cierra el menu de navegación.
+const menuNav = document.querySelector('nav'); // Traigo el elemento de navegación con sus respectivos links.
